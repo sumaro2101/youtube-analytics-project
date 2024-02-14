282fca9ab739e91c5be45822bd3fb04a85919fc4
Alex Pavlov <yukiu217@gmail.com>
2024-02-14 14:41:51 -0800

    add try-except in init Video

--- src/video.py
+++ src/video.py
@@ -4,13 +4,26 @@ import json
 class Video(Channel):
     
     def __init__(self, id_video: str) -> None:
-        super()._validate_id(id_video)
-        self.__id_video = id_video
-        self.__video_responce = super()._build_response_statistics_video(self.__id_video)
-        self.__name_video = self.__video_responce['items'][0]['snippet']['title']
-        self.__view_count = self.__video_responce['items'][0]['statistics']['viewCount']
-        self.__like_count = self.__video_responce['items'][0]['statistics']['likeCount']
-     
+        try:
+            super()._validate_id(id_video)
+            self.__id_video = id_video
+            self.__video_responce = super()._build_response_statistics_video(self.__id_video)
+            self.__title = self.__video_responce['items'][0]['snippet']['title']
+            self.__view_count = self.__video_responce['items'][0]['statistics']['viewCount']
+            self.__like_count = self.__video_responce['items'][0]['statistics']['likeCount']
+        except:
+            self.__title = None
+            self.__view_count = None
+            self.__like_count = None
+            
+    @property
+    def title(self):
+        return self.__title
+    
+    @property
+    def like_count(self):
+        return self.__like_count
+    
     def __str__(self) -> str:
         return self.__name_video
     
@@ -21,7 +34,7 @@ class Video(Channel):
     if not isinstance(value, (list, dict))]}
 {self.__class__.__name__}
 {self.__id_video}
-{self.__name_video}
+{self.__title}
 {self.__view_count}
 {self.__like_count}
 {json.dumps(self.__video_responce, ensure_ascii=False, indent=2)}'''
